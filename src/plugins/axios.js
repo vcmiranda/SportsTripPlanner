714943faa191610de240421d8255f69ab65b8644
vcmiranda <vcmiranda@outlook.com>
2018-11-30 00:11:48 -0500

    Updated api service, keys and credentials

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,13 +4,12 @@
 import Vue from 'vue';
 import axios from "axios";
 
-// Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
 let config = {
   baseURL: process.env.VUE_APP_API,
+  auth: {
+    username: process.env.VUE_APP_API_USER,
+    password: process.env.VUE_APP_API_PASS,
+  },
 };
 
 const _axios = axios.create(config);
