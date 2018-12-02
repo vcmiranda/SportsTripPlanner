2f13f9e36136cc3564bca1651c34d6b1a282145f
vcmiranda <vcmiranda@outlook.com>
2018-12-01 21:16:22 -0500

    This component handles base layout, setting background image, adding map and command bar components to landing page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div>
-    <Landing/>
-  </div>
+  <v-container fluid class="ma-0 pa-0">
+    <v-img :src="require('../assets/trophies.jpg')" :height="height">
+      <v-layout wrap row align-center>
+        <v-flex xs12 text-xs-center>
+          <Landing/>
+        </v-flex>
+        <v-flex xs10 offset-xs1 sm8 offset-sm2>
+          <v-card class="gMap-container">
+            <Map class="gMap"/>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-img>
+  </v-container>
 </template>
 
 <script>
 import Landing from '../components/Landing.vue';
+import Map from '../components/Map.vue';
 
 export default {
   name: 'Home',
   components: {
     Landing,
+    Map,
   },
   data: () => ({
+    height: '100vh',
   }),
 };
 </script>
+
+<style>
+.gMap-container {
+  height: 75vh;
+}
+.gMap {
+ height: 100%;
+ width: 100%;
+}
+</style>
