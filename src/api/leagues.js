2e796bad17aeb185ab81ef1866b9e946c892efbe
vcmiranda <vcmiranda@outlook.com>
2018-12-01 21:13:28 -0500

    Updated api call, removing unnecessary call, and adding comment.

--- src/api/leagues.js
+++ src/api/leagues.js
@@ -1,8 +1,12 @@
 export default {
-  getTeams(league) {
-    return window.axios.get(`/${league}/2018-2019-regular/team_stats_totals.json`);
-  },
-  getMatches(league, team, dates) {
+  /**
+   * @desc Counts number of registrations according with param, add new attribute to each element, and return array
+   * @param {string} league - League to be used (MLB, NBA, NFL, NHL)
+   * @param {string} team - Teams(s) to filter response
+   * @param {string} dates - Date(s) to filter response
+   * @returns {array} Returns an array with matches from chosen league, filtered or not by team(s) and date(s)
+   */
+  getSchedule(league, team, dates) {
     let path = `/${league}/2018-2019-regular/games.json`;
     if (team && dates) {
       path += `?team=${team}&date=${dates}`;
@@ -11,7 +15,6 @@ export default {
     } else if (dates) {
       path += `?date=${dates}`;
     }
-    // console.log(path);
     return window.axios.get(path);
   },
 };
