9a35dab0a58f8fb06a9c1fd896719574b9c23c4f
vcmiranda <vcmiranda@outlook.com>
2018-12-01 21:16:50 -0500

    Sets all commands to be used in this app.

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,183 +1,213 @@
 <template>
   <v-container fluid class="ma-0 pa-0">
-    <v-img :src="require('../assets/trophies.jpg')" :height="height">
-      <v-layout wrap row align-center fill-height>
-        <v-flex xs12 text-xs-center>
-          <v-layout wrap row justify-center>
-            <v-flex xs10 sm4 md2 class="mx-5">
-              <v-select
-                :items="leagues"
-                v-model="league"
-                item-text="label"
-                item-value="code"
-                label="League"
+    <v-layout wrap row align-center fill-height>
+      <v-flex xs12 text-xs-center>
+        <v-layout wrap row justify-center>
+          <v-flex xs10 sm4 md2 class="mx-5">
+            <v-select
+              :items="leagues"
+              :value="leagueSelected"
+              @input="setLeagueSelected"
+              item-text="label"
+              item-value="code"
+              label="League"
+              solo
+            />
+          </v-flex>
+          <v-flex xs10 sm4 md2 class="mx-5">
+            <v-select
+              :items="teamsList"
+              v-model="teamsSelected"
+              item-text="team"
+              item-value="abbreviation"
+              label="Team"
+              multiple
+              solo
+              :disabled="!leagueSelected"
+            >
+              <template
+                slot="selection"
+                slot-scope="{ item, index }"
+              >
+                <v-chip v-if="index === 0">
+                  <span>{{ item.abbreviation }}</span>
+                </v-chip>
+                <span
+                  v-if="index === 1"
+                  class="grey--text caption"
+                >(+{{ teamsSelected.length - 1 }} others)</span>
+              </template>
+            </v-select>
+          </v-flex>
+          <v-flex xs10 sm4 md2 class="mx-5">
+            <v-menu
+              ref="menuStart"
+              :close-on-content-click="false"
+              v-model="menuStart"
+              :nudge-right="40"
+              lazy
+              transition="scale-transition"
+              offset-y
+              full-width
+              min-width="290px"
+              :disabled="!leagueSelected"
+            >
+              <v-text-field
+                slot="activator"
+                v-model="dateStartSelected"
+                label="Start Date"
                 solo
-              />
-            </v-flex>
-            <v-flex xs10 sm4 md2 class="mx-5">
-              <v-select
-                :items="teams"
-                v-model="team"
-                item-text="team"
-                item-value="abbreviation"
-                label="Team"
-                multiple
+                readonly
+              ></v-text-field>
+              <v-date-picker
+                ref="picker"
+                v-model="dateStartSelected"
+                min="2018-08-01"
+                max="2019-07-31"
+                @change="saveDateStart"
+              ></v-date-picker>
+            </v-menu>
+          </v-flex>
+          <v-flex xs10 sm4 md2 class="mx-5">
+            <v-menu
+              ref="menuEnd"
+              :close-on-content-click="false"
+              v-model="menuEnd"
+              :nudge-right="40"
+              lazy
+              transition="scale-transition"
+              offset-y
+              full-width
+              min-width="290px"
+              :disabled="!leagueSelected"
+            >
+              <v-text-field
+                slot="activator"
+                v-model="dateEndSelected"
+                label="End Date"
                 solo
-                :disabled="!league"
-              >
-                <template
-                  slot="selection"
-                  slot-scope="{ item, index }"
-                >
-                  <v-chip v-if="index === 0">
-                    <span>{{ item.abbreviation }}</span>
-                  </v-chip>
-                  <span
-                    v-if="index === 1"
-                    class="grey--text caption"
-                  >(+{{ team.length - 1 }} others)</span>
-                </template>
-              </v-select>
-            </v-flex>
-            <v-flex xs10 sm4 md2 class="mx-5">
-              <v-menu
-                ref="menuStart"
-                :close-on-content-click="false"
-                v-model="menuStart"
-                :nudge-right="40"
-                lazy
-                transition="scale-transition"
-                offset-y
-                full-width
-                min-width="290px"
-                :disabled="!league"
-              >
-                <v-text-field
-                  slot="activator"
-                  v-model="dateStart"
-                  label="Start Date"
-                  solo
-                  readonly
-                ></v-text-field>
-                <v-date-picker
-                  ref="picker"
-                  v-model="dateStart"
-                  min="2018-08-01"
-                  max="2019-07-31"
-                  @change="saveDateStart"
-                ></v-date-picker>
-              </v-menu>
-            </v-flex>
-            <v-flex xs10 sm4 md2 class="mx-5">
-              <v-menu
-                ref="menuEnd"
-                :close-on-content-click="false"
-                v-model="menuEnd"
-                :nudge-right="40"
-                lazy
-                transition="scale-transition"
-                offset-y
-                full-width
-                min-width="290px"
-                :disabled="!league"
-              >
-                <v-text-field
-                  slot="activator"
-                  v-model="dateEnd"
-                  label="End Date"
-                  solo
-                  readonly
-                ></v-text-field>
-                <v-date-picker
-                  ref="picker"
-                  v-model="dateEnd"
-                  min="2018-08-01"
-                  max="2019-07-31"
-                  @change="saveDateEnd"
-                ></v-date-picker>
-              </v-menu>
-            </v-flex>
-            <v-flex xs12 class="mx-5">
-              <v-btn @click="getData()" :disabled="!league">Show Map</v-btn>
-              <v-btn @click="clearData()" :disabled="!league">Clear</v-btn>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-layout>
-    </v-img>
+                readonly
+              ></v-text-field>
+              <v-date-picker
+                ref="picker"
+                v-model="dateEndSelected"
+                min="2018-08-01"
+                max="2019-07-31"
+                @change="saveDateEnd"
+              ></v-date-picker>
+            </v-menu>
+          </v-flex>
+          <v-flex xs12 class="mx-5">
+            <v-btn @click="getSchedule()" :disabled="!leagueSelected">Show Map</v-btn>
+            <v-btn @click="clearData()" :disabled="!leagueSelected">Clear</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapState, mapGetters, mapMutations } from 'vuex';
 
 export default {
   data: () => ({
-    height: '100vh',
     leagues: [
-      { code: 'mlb', label: 'MLB' },
+      // { code: 'mlb', label: 'MLB' },
       { code: 'nba', label: 'NBA' },
       { code: 'nfl', label: 'NFL' },
       { code: 'nhl', label: 'NHL' },
     ],
-    league: null,
-    team: null,
-    dateStart: null,
-    dateEnd: null,
+    leagueSelected: null,
+    teamsSelected: null,
+    dateStartSelected: null,
+    dateEndSelected: null,
     menuStart: false,
     menuEnd: false,
   }),
   computed: {
-    ...mapGetters([
-      'teamsAll',
+    ...mapState([
+      'league',
     ]),
-    teams() {
-      if (this.league) {
-        return this.teamsAll(this.league);
+    ...mapGetters([
+      'teams',
+    ]),
+    teamsList() {
+      if (this.leagueSelected) {
+        return this.teams(this.leagueSelected);
       }
       return [];
     },
   },
   methods: {
-    getData() {
+    // Make mutations available in this component
+    ...mapMutations([
+      'setLeague',
+      'clearSchedule',
+    ]),
+    // Set league to be used, both locally and globally
+    setLeagueSelected(value) {
+      this.leagueSelected = value;
+      this.setLeague(value);
+    },
+    // It builds team(s) and date(s) filters, then it calls action that's going to retrieve matches.
+    getSchedule() {
       let teams = '';
       let dates = '';
-      if (this.team) {
-        this.team.forEach((team, i) => {
+      if (this.teamsSelected) {
+        this.teamsSelected.forEach((team, i) => {
           teams += team;
-          if (this.team.length !== i + 1) {
+          if (this.teamsSelected.length !== i + 1) {
             teams += ',';
           }
         });
       }
-      if (this.dateStart && this.dateEnd) {
-        dates = `from-${this.dateStart.replace(/-/g, '')}-to-${this.dateEnd.replace(/-/g, '')}`;
-      } else if (this.dateStart) {
-        dates = `since-${this.dateStart.replace(/-/g, '')}`;
-      } else if (this.dateEnd) {
-        dates = `until-${this.dateEnd.replace(/-/g, '')}`;
+      if (this.dateStartSelected && this.dateEndSelected) {
+        dates = `from-${this.dateStartSelected.replace(/-/g, '')}-to-${this.dateEndSelected.replace(/-/g, '')}`;
+      } else if (this.dateStartSelected) {
+        dates = `since-${this.dateStartSelected.replace(/-/g, '')}`;
+      } else if (this.dateEndSelected) {
+        dates = `until-${this.dateEndSelected.replace(/-/g, '')}`;
       }
-      this.$store.dispatch('getMatches', { league: this.league, team: teams, dates })
-        .then(() => { this.team = []; });
+      this.$store.dispatch('getSchedule', { league: this.league, team: teams, dates })
+        .then(() => {
+          this.teamsSelected = null;
+          this.$emit('component', 'Map');
+        });
     },
+    // It saves date to the correct element
     saveDateStart(date) {
       this.$refs.menuStart.save(date);
     },
+    // It saves date to the correct element
     saveDateEnd(date) {
       this.$refs.menuEnd.save(date);
     },
+    clearData() {
+      this.leagueSelected = null;
+      this.teamsSelected = null;
+      this.dateStartSelected = null;
+      this.dateEndSelected = null;
+      this.clearSchedule();
+    },
   },
   watch: {
-    league: {
+    // Every time user chooses a league, it calls an action to load teams list
+    leagueSelected: {
       handler(value) {
-        this.team = null;
-        this.$store.dispatch('getTeams', value);
+        if (value) {
+          this.teamsSelected = null;
+          this.$store.dispatch('getTeams', value);
+          this.$emit('league', value);
+        }
       },
       deep: true,
     },
+    // Starts date picker on Month
     menuStart(value) {
       value && this.$nextTick(() => { this.$refs.picker.activePicker = 'MONTH'; });
     },
+    // Starts date picker on Month
     menuEnd(value) {
       value && this.$nextTick(() => { this.$refs.picker.activePicker = 'MONTH'; });
     },
